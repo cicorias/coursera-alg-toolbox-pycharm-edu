806121b0e35f8ffec77ee592a7e5a400710165a7
Shawn Cicoria <github@cicoria.com>
2021-06-27 21:52:50 -0400

    first fib cone

--- Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
+++ Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
@@ -13,9 +13,32 @@ def fibonacci_number_naive(n):
 def fibonacci_number(n):
     assert 0 <= n <= 45
 
-    type here
+    if n <= 1:
+        return n
+
+    rv = 0
+    last = 0
+    current = 1
+    for _ in range(n - 1):
+        #2 =  0  +   1
+        rv = last + current
+        last = current
+        current = rv
+
+    return rv
 
 
 if __name__ == '__main__':
     input_n = int(input())
     print(fibonacci_number(input_n))
+
+
+
+
+# print(fibonacci_number_naive(2))
+# print(fibonacci_number_naive(3))
+# print(fibonacci_number_naive(5)) #5
+# print(fibonacci_number_naive(6)) #8
+#
+# print(fibonacci_number(5)) #5
+# print(fibonacci_number(6)) #8
